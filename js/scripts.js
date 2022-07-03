0d091bb625786ed861165e4a019d07619565cb5b
Niko Rivas <rivasniko@gmail.com>
2022-07-02 22:36:07 -0700

    pass replace 3 with will you be my neighbor test

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 function numList(num1) {
   const numArray = [];
   for (let i = 0; i <= num1; i++) [numArray.push(i)];
+  return numArray;
+}
+
+function replaceNumber(num) {
+  let numArray = numList(num);
+  numArray.forEach(function (num1, i) {
+    if (num1 === 3) numArray[i] = "Won't you be my neighbor";
+  });
   console.log(numArray);
 }
